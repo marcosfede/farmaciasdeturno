d91ad143bf1edd62ffc40c4700c0e660d1ceba7d
Federico Marcos <federico.marcos@celerative.com>
2019-01-20 15:17:14 -0300

    fix host

--- packages/scrapper/ormconfig.js
+++ packages/scrapper/ormconfig.js
@@ -1,8 +1,8 @@
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, NODE_ENV } = process.env
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, NODE_ENV } = process.env
 
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'postgres',
   port: 5432,
   username: 'test',
   password: 'test',
