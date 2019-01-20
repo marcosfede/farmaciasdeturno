b9560d1a1db84bab7763ebdaef3dbfa1a705fedd
Federico Marcos <federico.marcos@celerative.com>
2019-01-20 03:38:49 -0300

    gatsby initial boilerplate

---
+++ packages/client/src/components/layout.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import * as PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+import './layout.css'
+
+interface IProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: IProps) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={(data) => (
+      <>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
