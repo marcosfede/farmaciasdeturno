b6e5698f6057da3e53f5ff269421e4068d13c7e2
Federico Marcos <federico.marcos@celerative.com>
2019-07-07 20:33:21 -0300

    integrating ionic

--- packages/client/plugins/gatsby-plugin-antd-mobile/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.onCreateBabelConfig = ({ actions }, { style }) => {
-  actions.setBabelPlugin({
-    name: `babel-plugin-import`,
-    options: {
-      libraryName: 'antd-mobile',
-      style: style === true ? style : 'css',
-    },
-  })
-}
