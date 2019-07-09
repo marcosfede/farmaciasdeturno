508e02f5eaec7ffb369e4edecd1031009dd3110a
Federico Marcos <federico.marcos@celerative.com>
2019-07-09 16:13:04 -0300

    prev changes

--- packages/client/src/utils/dynamicClassName.ts
+++ packages/client/src/utils/dynamicClassName.ts
@@ -1,5 +1,8 @@
-export default function dynamicClassName(object) {
-  return Object.entries(object)
+export function dynamicClassName(dynamicClasses: { [x: string]: boolean }, staticClasses: string = ''): string {
+  const extra =
+    staticClasses.split(' ').length > 0 ? staticClasses.split(' ').map((cls): [string, boolean] => [cls, true]) : []
+  return Object.entries(dynamicClasses)
+    .concat(extra)
     .reduce((acc, [key, value]) => (!!value ? acc.concat(key) : acc), [] as string[])
     .join(' ')
 }
